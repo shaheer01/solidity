d52af0717afca5861bf2bf900732aec6aa5d04e6
shaheer01 <shaheer.m@Shaheers-MacBook-Pro.local>
2023-04-23 20:02:56 +0530

    Enum example

--- MyContract.sol
+++ MyContract.sol
@@ -3,16 +3,27 @@ pragma solidity ^0.8.0;
 
 contract MyContract {
     string value;
+    enum State {waiting, active, ready};
+    string public state;
 
     constructor(){
         value = "myValue";
+        state = State.waiting;
     }
-
+    //Instead of this you can make the line #5 as public i.e string public value and then you dont need the below function.
     function get() public view returns(string memory) {
         return value;
     }
 
     function set(string memory _value) public {
         return value = _value;
+    }
+
+    function activate() public {
+        state = State.active;
+    }
+
+    function isActive() public view  returns(bool){
+        return state == State.active;
     }
 }

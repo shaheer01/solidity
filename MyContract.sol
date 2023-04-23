169d44901ef7a4ed29c56083344109ce2556d66e
shaheer01 <shaheer.m@Shaheers-MacBook-Pro.local>
2023-04-23 19:38:56 +0530

    Fixing the issue

--- MyContract.sol
+++ MyContract.sol
@@ -8,11 +8,11 @@ contract MyContract {
         value = "myValue";
     }
 
-    function get() public view returns(string) {
+    function get() public view returns(string memory) {
         return value;
     }
 
-    function set(string _value) public {
+    function set(string memory _value) public {
         return value = _value;
     }
 }
